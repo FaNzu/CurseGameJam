ca34c73d7ce147a579d6568a1e479eec510cfe71
Ask <34611138+FaNzu@users.noreply.github.com>
2024-05-10 17:26:43 +0200

    initial commit

---
+++ Menus/MainMenu.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _ready():
+	pass
+
+func _on_exit_button_pressed():
+	get_tree().quit()
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://Levels/FetchGame.tscn")
+
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file("res://Menus/credits.tscn")
