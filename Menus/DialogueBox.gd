bd533dd26cbb64dd7a8e65f72a91351b6fc65892
Ask <34611138+FaNzu@users.noreply.github.com>
2024-05-11 12:39:11 +0200

    ez dialogue working

---
+++ Menus/DialogueBox.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+@onready var choice_button_scn = preload("res://Utility/choice_button.tscn")
+var choice_buttons: Array[Button] = []
+
+func _ready():
+	pass
+	#add_choice("this is a choice number 0")
+	#add_choice("this is a choice number 1")
+
+func clear_dialogue_box():
+	$VBoxContainer/Text.text = ""
+	for choice in choice_buttons:
+		$VBoxContainer.remove_child(choice)
+	choice_buttons = []
+
+func add_text(text: String):
+	$VBoxContainer/Text.text = text
+	
+func add_choice(choice_text: String):
+	var button_obj: choice_button = choice_button_scn.instantiate()
+	button_obj.choice_index = choice_buttons.size()
+	choice_buttons.push_back(button_obj)
+	button_obj.text = choice_text
+	button_obj.choice_selected.connect(_on_choice_selected)
+	$VBoxContainer.add_child(button_obj)
+
+func _on_choice_selected(choice_index: int):
+	print(choice_index)
+	($"../EzDialogue" as EzDialogue).next(choice_index)
+	pass
--- Menus/credits.gd
+++ Menus/credits.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
-
+@export var dialogue_json: JSON
+@onready var state = {}
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	($EzDialogue as EzDialogue).start_dialogue(dialogue_json, state)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -11,3 +12,8 @@ func _process(delta):
 
 func _on_credits_button_pressed():
 	get_tree().change_scene_to_file("res://Menus/main_menu.tscn")
+
+func _on_ez_dialogue_dialogue_generated(response: DialogueResponse):
+	$DialogueBox.add_text(response.text)
+	for choice in response.choices:
+		$DialogueBox.add_choice(choice)
